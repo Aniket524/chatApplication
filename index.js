32bc7cd51bea591b033a6022cbf8e3dd804f6989
Aniket Karpe <81089019+Aniket524@users.noreply.github.com>
2024-02-16 15:47:20 +0530

    first change to be consider

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('this is the main page of the application')
+})
+
+app.get('/demo',(req,res)=>{
+    res.send("this is the demo page os the application")
+})
+
+app.listen(3000,()=>{
+    console.log('app is running on port 3000')
+})
